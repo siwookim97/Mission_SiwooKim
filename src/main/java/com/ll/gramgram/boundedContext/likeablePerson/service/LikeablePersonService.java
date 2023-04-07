e22faaed7f0d5aa72951043462b0406edf761611
siwookim97 <tldn12gh@gmail.com>
2023-04-07 14:07:55 +0900

    [Refactor] : delete 메서드 @Transactional 어노테이션 추가

--- src/main/java/com/ll/gramgram/boundedContext/likeablePerson/service/LikeablePersonService.java
+++ src/main/java/com/ll/gramgram/boundedContext/likeablePerson/service/LikeablePersonService.java
@@ -46,8 +46,7 @@ public class LikeablePersonService {
         return RsData.of("S-1", "입력하신 인스타유저(%s)를 호감상대로 등록되었습니다.".formatted(username), likeablePerson);
     }
 
-    // 쿼리가 한번만 실행되므로 원자성을 유지할 필요가 없다
-    // @Transactional
+    @Transactional
     public RsData<LikeablePerson> deleteLike(Member member, long likeableId) {
         // 예외1
         Optional<LikeablePerson> optionalLikeablePerson = likeablePersonRepository.findById(likeableId);
