8063a995b745783049fe05a82b460d27c3a7499b
siwookim97 <tldn12gh@gmail.com>
2023-05-01 17:58:42 +0900

    [Refactor] 리팩토링

--- src/main/java/com/ll/gramgram/base/security/CustomUserDetailsService.java
+++ src/main/java/com/ll/gramgram/base/security/CustomUserDetailsService.java
@@ -18,8 +18,7 @@ public class CustomUserDetailsService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        Member member = memberRepository.findByUsername(username).orElseThrow(
-                () -> new UsernameNotFoundException("username(%s) not found".formatted(username)));
+        Member member = memberRepository.findByUsername(username).orElseThrow(() -> new UsernameNotFoundException("username(%s) not found".formatted(username)));
 
         return new User(member.getUsername(), member.getPassword(), member.getGrantedAuthorities());
     }
